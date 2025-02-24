993ad3a869f7c955da522ba20f6ed488babe215c
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-24 13:30:18 +0000

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -3,3 +3,8 @@ import random
 def hangman_game():
     words = ["python", "hangman", "programming", "eternal", "concrete"]
     word = random.choice(words)
+    guessed = set()
+    attempts = 7
+    display_word = ["_" for _ in word]
+
+    
